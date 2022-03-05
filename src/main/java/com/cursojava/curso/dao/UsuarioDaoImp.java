165b2c41e9363d243d2e2c4d978a06ec10a6f4af
AdriFC <adri86vigo@gmail.com>
2022-03-05 11:44:07 +0100

    Configuración repository con Hibernate

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -2,17 +2,25 @@ package com.cursojava.curso.dao;
 
 import com.cursojava.curso.models.Usuario;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@Transactional
 
 public class UsuarioDaoImp implements UsuarioDao {
 
+    @PersistenceContext
+    private EntityManager entitymanager;
+
     @Override
     public List<Usuario> getUsuarios() {
-        return null;
+        String query = "FROM Usuario";
+        return entitymanager.createNamedQuery(query).getResultList();
     }
 
 }
